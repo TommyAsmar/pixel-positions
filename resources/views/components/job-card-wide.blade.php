2f2c42db9af263d43772def74bff544947e8c575
Tommy El Asmar <tommyelasmar@gmail.com>
2025-07-29 00:06:55 +0300

    my first commit

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,19 @@
+<div
+    class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-600 transition-colors duration-300 group">
+    <div>
+        <x-employer-logo></x-employer-logo>
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+    </div>
+
+    <div class="">
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</div>
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pixel Postions</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@400;500;600&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body class="bg-black text-white font-hanken-grotesk">
+    <div class="px-10">
+        <nav class="flex justify-between items-center p-10 py-4 border-b border-white/10">
+            <div>
+                <a href="">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
+
+            <div class="space-x-6 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+
+            <div>
+                <a href="">Post a Job</a>
+            </div>
+        </nav>
+
+        <main class="mt-10 max-w-[986px] mx-auto"> {{ $slot }}</main>
+    </div>
+</body>
+
+</html>
